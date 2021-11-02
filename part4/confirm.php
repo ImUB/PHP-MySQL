78dc6e321ea5ea8fe5cbfefcf1aa2b97d430120a
imub <dladbql1234@gmail.com>
2021-11-02 21:07:21 +0900

    배열 데이터 post로 넘겨주기

--- part4/confirm.php
+++ part4/confirm.php
@@ -5,21 +5,29 @@
     <title>PHP 테스트</title>
   </head>
   <body>
-    확인 화면
+    확인 화면<BR><BR>
     <form name="form1" action="view.php" method="post">
       <?php
       print "이름:";
-      print $_POST["onname"];
-      print "<BR><BR>";
+      print $_POST["onname"]."<BR>";
+      if (isset($_POST["hobby"])) {
+        $hobby = implode('와 ', $_POST["hobby"]);
+        print "저의 취미는 ";
+        print $hobby;
+        print "입니다.<BR><BR>";
+      } else {
+        print "저의 취미는 없습니다.<BR><BR>";
+      }
       print "본문 :<BR>";
       print nl2br($_POST["bonmun"]);
       ?>
       <BR>
-      <input type="submit" value="확인">
+      <input type="submit" name="confirm" value="확인">
       <input type="submit" name="back" value="돌아가기">
       <input type="hidden" name="onname" value="<?=$_POST["onname"]?>">
       <input type="hidden" name="bonmun" value="<?=$_POST["bonmun"]?>">
       <input type="hidden" name="user_id" value="<?=$_POST["user_id"]?>">
+      <input type="hidden" name="hobby"  value="<?=$hobby?>">
     </form>
   </body>
 </html>
--- part4/view.php
+++ part4/view.php
@@ -7,12 +7,41 @@
   <body>
     <?php
       if (isset($_POST["confirm"])) {
-        print $_POST["onname"]."님으로부터의 메시지<BR><BR>";
-        print "본문 :<BR>";
-        print nl2br($_POST["bonmun"]);
-      } elseif (isset($_POST["back"])) {
-        #P166부터 다시해보자
-      }
     ?>
+    <?php
+      print $_POST["onname"]."님으로부터의 메시지<BR><BR>";
+      if (!empty($_POST["hobby"])) {
+        print "저의 취미는 ";
+        print $_POST["hobby"];
+        print "입니다.<BR><BR>";
+      } else {
+        print "저의 취미는 없습니다.<BR><BR>";
+      }
+      print "<BR><BR>";
+      print "본문 :<BR>";
+      print nl2br($_POST["bonmun"]);
+    ?>
+    <?php
+      } elseif (isset($_POST["back"])) {
+     ?>
+     <FONT size="4">텍스트 송신 테스트</FONT>
+     <form name="form1" action="confirm.php" method="post">
+       이름: <input type="text" name="onname"><BR>
+       나의 취미 : <BR>
+       <input type="checkbox" name="hobby[]" value="스포츠">스포츠<BR>
+       <input type="checkbox" name="hobby[]" value="영화감상">영화감상<BR>
+       <input type="checkbox" name="hobby[]" value="독서">독서<BR><BR>
+       본문: <BR>
+       <textarea name="bonmun" rows="8" cols="80"></textarea><BR>
+       <input type="submit" value="송신">
+       <input type="hidden" name="user_id" value="0001">
+    <?php
+      }else{
+     ?>
+     오류입니다. <BR>
+     <a href="form.html">form.html</a>로 돌아갑니다.
+    <?php
+      }
+     ?>
   </body>
 </html>
